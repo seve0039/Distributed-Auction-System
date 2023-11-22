0d18cbeef9e489ce660fcef2242dd473ca236c0a
seve0039 <sevm@itu.dk>
2023-11-22 12:48:13 +0100

    added connectio between client and server

--- Client/client.go
+++ Client/client.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"bufio"
 
+	gRPC "github.com/seve0039/Distributed-Auction-System.git/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// var clientsName = flag.String("name", "Placeholder", "Sender's name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var server gRPC.AuctionServiceClient
+var ServerConn *grpc.ClientConn
+
+func main() {
+	flag.Parse()
+	connectToServer()
+
+	for {
+
+	}
+
+}
+
+func connectToServer() {
+	opts := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
+	if err != nil {
+		log.Fatalf("Fail to Dial : %v", err)
+		fmt.Println("Failed to connect to server")
+	}
+
+	server = gRPC.NewAuctionServiceClient(conn)
+	ServerConn = conn
+	fmt.Println("Connected to server")
+}
+
+func sendMessage(amount int64) {
+	_, _ = server.Bid(context.Background(), &gRPC.BidAmount{
+		Id: 1, Amount: amount, Name: "John Doe",
+	})
+}
+
+func handleCommand() { //Handle commands from user input via the terminal
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("--- Please make your bid ---")
+		for {
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+		}
+		if input == "status" {
+			fmt.Println("The highest bid for now is :") //TODO: Get results
+		} else {
+			
+		}
+
+	}
 }
--- Server/server.go
+++ Server/server.go
@@ -1,7 +1,53 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	gRPC "github.com/seve0039/Distributed-Auction-System.git/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+type Server struct {
+	gRPC.UnimplementedAuctionServiceServer
+	name string
+	port string
+	//participants map[string]gRPC.AuctionService_BroadcastServer
+}
+
+var serverName = flag.String("name", "default", "Server's name")
+var port = flag.String("port", "5400", "Server port")
+
 func main() {
+	flag.Parse()
+	launchServer()
+}
+
+func launchServer() {
+	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", *port, err)
+	}
+
+	auctionServer := grpc.NewServer()
+	server := &Server{
+		name: *serverName,
+		port: *port,
+		//participants: make(map[string]gRPC.AuctionService_BroadcastServer),
+	}
+
+	gRPC.RegisterAuctionServiceServer(auctionServer, server)
+	log.Printf("NEW SESSION: Server %s: Listening at %v\n", *serverName, list.Addr())
+
+	if err := auctionServer.Serve(list); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
+}
+
+func (s *Server) Bid(context context.Context, criticalSectionRequest *gRPC.BidAmount) (*emptypb.Empty, error) {
+
 }
